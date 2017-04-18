a8e26375e0eddd4d40b382acdafec4e7b61a6de8
wp0577 <196448716@qq.com>
2017-04-18 15:39:57 -0400

    two-sum
    1th day

---
+++ leetcode/twoSum.java
@@ -0,0 +1,29 @@
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] res=new int[2];
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(target==nums[i]+nums[j]){
+                    res[0]=i;
+                    res[1]=j;
+                }
+            }
+        }
+        return res;
+    }
+}
+
+
+public int[] twoSum(int[] numbers, int target) {
+    int[] result = new int[2];
+    Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+    for (int i = 0; i < numbers.length; i++) {
+        if (map.containsKey(target - numbers[i])) {
+            result[1] = i + 1;
+            result[0] = map.get(target - numbers[i]);
+            return result;
+        }
+        map.put(numbers[i], i + 1);
+    }
+    return result;
+}
