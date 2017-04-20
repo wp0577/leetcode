9ed6f4a48afc2a5527c0cabf638c8fc756abe2ea
wp0577 <wu.pa@icd25lp1y5f8jc.nunet.neu.edu>
2017-04-20 19:19:25 -0400

    LongestString
    3th

---
+++ LongestString.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        HashMap<Character, Integer> hm=new HashMap<Character, Integer>();
+        int max=0;
+        for(int i=0, j=0; j<s.length(); j++){
+            if(hm.containsKey(s.charAt(j))){
+                if(hm.get(s.charAt(j))>=i) i=hm.get(s.charAt(j))+1;
+              
+            }
+            hm.put(s.charAt(j),j);
+            max=Math.max(max,j-i+1);
+        }
+        return max;
+    }
+}
